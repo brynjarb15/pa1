f991c7a7849d59c92a758e737eaed445b43ef1a5
brynjarb15 <brynjarb15@skel.ru.is>
2017-09-09 00:59:12 +0000

    adalega bara print skipanir lok fostudags

--- src/tftpd.c
+++ src/tftpd.c
@@ -6,7 +6,8 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[])
-{
+{	
+    printf("starting \n");
     int sockfd;
     struct sockaddr_in server, client;
     char message[512];
@@ -15,36 +16,45 @@ int main (int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
-
+    
     // Network functions need arguments in network byte order instead
     // of host byte order. The macros htonl, htons convert the values.
     server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons();
+    server.sin_port = htons(59545);
     int bindStatus = bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
-	
-	if(bindStatus < 0){
-		//error
-		printf("bind failed \n");
-	}
-
+    
+    if(bindStatus < 0){
+        //error
+	printf("bind failed \n");
+    }
+    
     for (;;) {
+		printf("beginning of for loop \n");
         // Receive up to one byte less than declared, because it will
         // be NUL-terminated later.
         socklen_t len = (socklen_t) sizeof(client);
+	
         ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1,
                              0, (struct sockaddr *) &client, &len);
-
-        message[n] = '\0';
-        fprintf(stdout, "Received:\n%s\n", message);
+	printf("len: %tu \n", len);
+	//nr 1 and 2 are the opcode
+	char* fileName = 2 + message;
+       	printf("FileName: %s\n", fileName);
+	int fileNameLength = strlen(fileName);
+	char* mode = 2 + fileNameLength + 1 + message;
+	printf("mode: %s\n", mode);
+	message[n] = '\0';
+       	fprintf(stdout, "Received:\n%s\n", message);
         fflush(stdout);
-
+		
         // convert message to upper case.
         for (int i = 0; i < n; ++i) {
             message[i] = toupper(message[i]);
         }
-
+	printf("message: %s \n", message);
         sendto(sockfd, message, (size_t) n, 0,
                (struct sockaddr *) &client, len);
+	printf("end of for loop \n");
     }
 	
 }
